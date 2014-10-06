978f07da35f94540ac7aca90f52f8dca0fbe92b4
Ikenna <iomekam3@gatech.edu>
2014-10-06 03:43:10 -0400

    Adding index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
 
-$connectionInfo = array(\"UID\" => \"buzzDB@hqwkbgz8vt\", \"pwd\" => \"{your_password_here}\", \"Database\" => \"buzzDB\", \"LoginTimeout\" => 30, \"Encrypt\" => 1)";
-$serverName = "tcp:hqwkbgz8vt.database.windows.net,1433\"";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$con = connect()
 ?>
