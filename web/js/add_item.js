f9db2d4eb71bfd3bd09fe7b75f43002ae385d9d6
Hayden Erickson <herickson3@gatech.edu>
2014-10-06 18:03:13 -0400

    Hayden I committed this page so it can communicate with the db

---
+++ web/js/add_item.js
@@ -0,0 +1,33 @@
+// this is the java script for the add item page 
+
+$( '#image' ).on( 'click', add_image );
+
+function add_image(){
+    $( '#upload_image' ).click(); 
+    console.log( 'upload new image' ); 
+}
+
+function add_new_item()
+{
+    $.ajax({
+        url : "items.php",
+        type: 'POST',
+        data : {
+            name:        $( '#title' ).val(), 
+            price:       $( '#asking_price' ).val(), 
+            description: $( '#description' ).val(), 
+            user_id:     'stuffisgood', 
+            image_url:   'blah.jpg' 
+        }, 
+        success: function(data, textStatus, jqXHR)
+        {
+            console.log( 'item sent successfully' );
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+            alert( 'error ' + errorThrown );
+        }
+    });
+}
+
+
