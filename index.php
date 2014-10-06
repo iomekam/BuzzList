6a8568a5dc2ac96eb4ad57db7225297d4909f6d9
Ikenna <iomekam3@gatech.edu>
2014-10-06 03:45:33 -0400

    Adding index.html

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 
+function connect()
+{
+	// DB connection info
+	$host = "tcp:hqwkbgz8vt.database.windows.net";
+	$user = "buzzDB";
+	$pwd = "ikenna@MAS";
+	$db = "buzzDB";
+	try{
+		$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch(Exception $e){
+		die(print_r($e));
+	}
+	return $conn;
+}
+
+
 $con = connect()
 ?>
