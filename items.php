bd3416beea9128fc12fda528a09b09b1b0d357cf
Ikenna <iomekam3@gatech.edu>
2014-10-06 13:37:45 -0400

    Adding index.html

--- items.php
+++ items.php
@@ -3,7 +3,14 @@ include_once("dbconnect.php");
 
 $db = new DBConnect();
 
-
+if(isset($_GET['item_id'])
+{
+	
+}
+else if(is_array($_GET))
+{
+	
+}
 	
 function getItem($itemID)
 {
@@ -12,7 +19,7 @@ function getItem($itemID)
 
 function getAllItems()
 {
-	$conn = db->connect();
+	$conn = $db->connect();
 	$sql = "SELECT * FROM item";
 	$stmt = $con->query($sql);
 	return $stmt->fetchAll(PDO::FETCH_NUM);
